ae10a9ebbf932b5ed0076f6009b21f4d81c17a9e
missionariazinha <ana.bumiller@escola.pr.gov.br>
2025-07-02 20:24:13 -0300

    aula 5 - 02/07

---
+++ script.js
@@ -0,0 +1,40 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
+
+function mostraPalavraChave() {
+    //alert("Fui clicado!");
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = processaTexto(texto);
+
+    campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto) {
+    //let palavras = texto.split(/\s+/);  "retirar espaços"
+    //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
+    let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+    const frequencias = contaFrequencias(palavras);
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    function ordenaPalavra(p1, p2){
+        return frequencia[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0,10);
+}
+    
+  function contaFrequencias(palavras)  {
+    let frequencias = {};
+
+    for ( let i of palavras) {
+        frequencias[i] = 0;
+        for ( let j of palavras) {
+            if(i == j ) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return palavras;
+}
